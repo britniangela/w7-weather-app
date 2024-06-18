2fb8960c7bb68628f6ebdc82957190888490c650
britniangela <66388266+britniangela@users.noreply.github.com>
2024-06-18 17:32:07 -0400

    updated fahrenheit to celsius toggle

--- src/index.js
+++ src/index.js
@@ -1,42 +1,24 @@
-// function getCelciusTemperature(response) {
-//   celsiusTemperature = document.querySelector(".app-temperature-unit");
-//   let celsiusTemp = Math.round(
-//     ((response.data.temperature.current - 32) * 5) / 9
-//   );
-//   celsiusTemperature.innerHTML = celsiusTemp;
-// }
+let temperature = null;
+function getCelciusTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#app-temperature-value");
+  console.log({ temperature });
+  temperatureElement.innerHTML = Math.round(((temperature - 32) * 5) / 9);
+  celsiusTemperature.classList.add("disabled");
+  fahrenheitTemperature.classList.remove("disabled");
+}
 
-// let celsiusTemperature = document.querySelector("#app-temperature-unit");
-// celsiusTemperature.addEventListener("click", getCelciusTemperature);
+let celsiusTemperature = document.querySelector(
+  "#app-temperature-celsius-unit"
+);
+celsiusTemperature.addEventListener("click", getCelciusTemperature);
 
-// function getWeather(response) {
-//   let temperatureElement = document.querySelector("#app-temperature-value");
-//   let temperature = Math.round(response.data.temperature.current);
-//   let cityElement = document.querySelector("#app-city");
-
-//   cityElement.innerHTML = response.data.city;
-//   temperatureElement.innerHTML = temperature;
-// }
-
-// function searchCity(city) {
-//   let apiKey = "58f3tbaa2acbfcb0981e0ecb4ba2453o";
-//   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
-//   axios.get(apiUrl).then(getWeather);
-// }
-
-// function handleSearchSubmit(event) {
-//   event.preventDefault();
-//   let searchInput = document.querySelector("#search-form-input");
-
-//   searchCity(searchInput.value);
-// }
-
-// let searchFormElement = document.querySelector("#search-form");
-// searchFormElement.addEventListener("submit", handleSearchSubmit);
 function getFahrenheitTemperature(event) {
   event.preventDefault();
-  units = "imperial";
-  searchCity(lastSearchedCity);
+  let temperatureElement = document.querySelector("#app-temperature-value");
+  temperatureElement.innerHTML = temperature;
+  celsiusTemperature.classList.remove("disabled");
+  fahrenheitTemperature.classList.add("disabled");
 }
 
 let fahrenheitTemperature = document.querySelector(
@@ -46,7 +28,7 @@ fahrenheitTemperature.addEventListener("click", getFahrenheitTemperature);
 
 function getWeather(response) {
   let temperatureElement = document.querySelector("#app-temperature-value");
-  let temperature = Math.round(response.data.temperature.current);
+  temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#app-city");
 
   cityElement.innerHTML = response.data.city;
@@ -55,7 +37,7 @@ function getWeather(response) {
 
 function searchCity(city) {
   let apiKey = "58f3tbaa2acbfcb0981e0ecb4ba2453o";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(getWeather);
 }
 
@@ -70,7 +52,6 @@ function handleSearchSubmit(event) {
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-let units = "imperial";
 let lastSearchedCity = "Orlando";
 
 searchCity(lastSearchedCity);
